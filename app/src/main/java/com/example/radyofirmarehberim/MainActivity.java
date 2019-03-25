fc3a18d45e25ce7fb45b4039e034d73d220adbcd
seymasa <seymasargil@gmail.com>
2019-03-25 15:17:46 +0300

    Environment variables prepared

---
+++ app/src/main/java/com/example/radyofirmarehberim/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.radyofirmarehberim;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_favori:
+                    mTextMessage.setText(R.string.title_favori);
+                    return true;
+                case R.id.navigation_tur:
+                    mTextMessage.setText(R.string.title_tur);
+                    return true;
+                case R.id.navigation_yerel:
+                    mTextMessage.setText(R.string.title_ulusal);
+                    return true;
+                case R.id.navigatio_ulusal:
+                    mTextMessage.setText(R.string.title_yerel);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
